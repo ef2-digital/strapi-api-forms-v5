5504fe015249143a33976fff64872a1b42d1b4a2
R. Geelen <roel-geelen@hotmail.com>
2026-03-05 15:07:06 +0100

    remove dist

--- dist/admin/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const index = require("../_chunks/index-Bdtsx0TG.js");
-module.exports = index.index;
--- dist/admin/index.mjs
+++
@@ -1,4 +0,0 @@
-import { i } from "../_chunks/index-YQBWBTss.mjs";
-export {
-  i as default
-};
